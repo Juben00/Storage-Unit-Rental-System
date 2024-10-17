283293cae47ee4043d0e8f0de4c7fdf6d58835e2
Joevin <joevinansoc870@gmail.com>
2024-10-17 11:32:08 +0800

    form fixes

--- components/addStorageForm.php
+++ components/addStorageForm.php
@@ -1,52 +1,56 @@
-<form class="bg-white p-6 rounded-lg shadow-lg" id="addStorageForm" enctype="multipart/form-data" method="POST"
-    action="upload.php">
+<form class="bg-white p-6 rounded-lg shadow-lg" id="addStorageForm" enctype="multipart/form-data">
     <div class="mb-4">
-        <label class="block text-gray-700 font-semibold mb-2" for="storageImages">
+        <label class="block text-gray-700 font-semibold mb-1" for="storageImages">
             Storage Images </label>
         <input type="file" id="storageImages" name="storageImages[]" multiple required
             class="border-2 w-full border-dashed border-gray-300 rounded-lg p-6 text-center" />
     </div>
 
-    <div class="mb-4">
-        <label class="block text-gray-700 font-semibold mb-2" for="productName">
-            Product Name </label>
-        <input
-            class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
-            id="storageName" name="storageName" placeholder="Storage Name" type="text" required />
+    <div class="mb-4 flex items-center gap-2">
+        <span class="w-full">
+            <label class="block text-gray-700 font-semibold mb-1" for="productName">
+                Storage Name </label>
+            <input
+                class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
+                id="storageName" name="storageName" placeholder="Storage Name" type="text" required />
+        </span>
+        <span class="w-2/5">
+            <label class="block text-gray-700 font-semibold mb-1" for="productName">
+                Storage Area <span class="text-xs font-normal text-red-500">*sqm</span></label>
+            <input
+                class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
+                id="storageName" name="storageArea" placeholder="Storage Area" type="text" required />
+        </span>
+
     </div>
 
     <div class="mb-4">
-        <label class="block text-gray-700 font-semibold mb-2" for="description"> Description </label>
-        <input
-            class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
-            id="description" name="description" placeholder="Storage Description" type="text" required />
+        <label class="block text-gray-700 font-semibold mb-1" for="description"> Description </label>
+        <textarea
+            class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600 resize-none"
+            id="description" name="description" placeholder="Storage Description" rows="4" required></textarea>
     </div>
 
-    <div class="mb-4">
-        <label class="block text-gray-700 font-semibold mb-2" for="category">
-            Category </label>
-        <select name="category" id="category"
-            class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
-            required>
-            <option value="" disabled selected>Select an Option</option>
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-        </select>
-    </div>
 
     <div class="mb-4">
-        <div class="flex items-center gap-2">
+        <input type="hidden" name="category" id="category"
+            class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600" />
+    </div>
+
+    <div class="flex items-center gap-2 mb-4">
+        <span class="flex flex-col w-full">
             <label class="block text-gray-700 font-semibold " for="stock"> Stock </label>
             <input
                 class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
                 id="stock" name="stock" placeholder="Storage Quantity" type="number" required />
+        </span>
 
+        <span class="flex flex-col w-full">
             <label class="block text-gray-700 font-semibold " for="price"> Price </label>
             <input
                 class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
                 id="price" name="price" placeholder="Storage Price" type="number" required />
-        </div>
+        </span>
     </div>
 
     <input type="submit" value="Add Storage"
